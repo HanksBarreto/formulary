8e09d06568cf99a7ab5d2585f4466d68822dcf98
HanksBarreto <57303623+HanksBarreto@users.noreply.github.com>
2020-06-21 20:40:44 -0500

    git
    git

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,16 @@
 
 
 class clienteRadius{
-    constructor(codigo, nombre, direccion, fecha, correo, marca, ip, megas){
+    constructor(codigo, nombre, direccion, correo, marca, megas, facturaccion,precio){
         this.codigo = codigo;
         this.nombre = nombre;
         this.direccion = direccion;
-        this.fecha = fecha;
         this.correo = correo;
         this.marca = marca;
-        this.ip = ip;
         this.megas = megas;
+        this.facturaccion = facturaccion;
+        this.precio = precio;
+        
       
     }
 }
@@ -23,12 +24,13 @@ class GUI{
                  <td scope="row" class="text-danger">${cliente.codigo}</td>   
                  <td scope="row" class="td">${cliente.nombre}</td>  
                  <td scope="row" class="td">${cliente.direccion}</td>  
-                 <td scope="row" class="td">${cliente.fecha}</td>   
                  <td scope="row" class="td">${cliente.correo}  </td> 
-                 <td scope="row" class="td">${cliente.marca}</td> 
-                 <td scope="row" class="td">${cliente.ip}</td>   
-                 <td scope="row" class="td megas text-info">${cliente.megas} </td>  
-               <td> <a href="#" class="btn btn-danger text-light" name="delete">Eliminar</a> </td>
+                 <td scope="row" class="td">${cliente.marca}</td>
+                 <td scope="row" class="td ">${cliente.megas} </td>  
+                 <td scope="row" class="td">${cliente.facturaccion}</td> 
+                 <td scope="row" class="td precio text-info">${cliente.precio} </td>   
+                  
+               <td> <a href="#" class="btn btn-danger text-light" name="delete">Eliminar</a></td>
         `;
         listClient.appendChild(element);
    
@@ -63,13 +65,12 @@ document.getElementById('form-cliente')
 const codigo = document.getElementById('codigo').value;
 const nombre = document.getElementById('nombre').value;
 const direccion = document.getElementById('direccion').value;
-const fecha = document.getElementById('fecha').value;
 const correo = document.getElementById('correo').value;
 const marca = document.getElementById('marca').value;
-const ip = document.getElementById('ip').value;
 const megas = document.getElementById('megas').value;
-
-const cliente = new clienteRadius(codigo, nombre, direccion, fecha, correo,  marca, ip, megas);
+const facturacion = document.getElementById('facturacion').value;
+const precio = document.getElementById('precio').value;
+const cliente = new clienteRadius(codigo, nombre, direccion, correo,  marca, megas, facturacion, precio);
     const ui = new GUI();
     //instanciando Metodos
     ui.addCliente(cliente);
